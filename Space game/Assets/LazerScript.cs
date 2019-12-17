02e3722ec0b73941dda5bd3a9bc85c81616258e6
Vladimir Krygin <nif315@gmail.com>
2019-12-18 01:55:54 +0300

    Shot texture and effects added to the scene, shot's movement implemented

---
+++ Space game/Assets/LazerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerScript : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.forward * speed; //заставляем выстрел двигаться только вперед
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
